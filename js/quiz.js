a56f9e1c8bc015b5cf080754f0bd3b2dd89835f8
dchen7321 <dchen7321@gmail.com>
2017-04-18 05:28:14 -0400

    Updated image repository for quiz

--- js/quiz.js
+++ js/quiz.js
@@ -1,89 +1,10 @@
 var canvas = document.getElementById("canvas1");
 var ctx = canvas.getContext("2d");
-var keyImage;
 var sizeFactor = 110;
 var mouse = {};
+var questionList = ["Are you a vegetarian?", "Are you feeling adventurous?", "Do you like shellfish?", "Want something traditional?", "Do you love pork?", "Want something sweet?", "Still hungry for dessert?"];
+var quizState = 0;
 var fadeAlpha = 0;
-
-function CanvasImage(imageID, position, infoID) {
-	this.img = document.getElementById(imageID);
-	this.aspectRatio = this.img.width / this.img.height;
-	this.h = sizeFactor;
-	this.w = this.h * this.aspectRatio;
-	this.position = position;
-	this.resize = 0;
-	this.alpha = 0;
-	this.info = document.getElementById(infoID);
-
-	this.updatePosition = function() {
-		if(position == 1) {
-			this.absolutex = (canvas[0].width / 2);
-			this.absolutey = 125;
-		}
-		if(position == 2) {
-			this.absolutex = 400;
-			this.absolutey = (canvas[0].height / 2) - 25;
-		}
-		if(position == 3) {
-			this.absolutex = canvas[0].width - 400;
-			this.absolutey = (canvas[0].height / 2) - 25;
-		}
-		if(position == 4) {
-			this.absolutex = (canvas[0].width / 2); 
-			this.absolutey = canvas[0].height - 175;
-		}
-		if(position == 5) {
-			this.absolutex = canvas[0].width - 300;
-			this.absolutey = 130;
-			this.h = 125;
-			this.w = this.h * this.aspectRatio;
-		}
-		this.x = this.absolutex - (this.w / 2);
-		this.y = this.absolutey - (this.h / 2);
-	}
-
-	this.updatePosition();
-
-	this.resizeUpdate = function() {
-		this.h = sizeFactor + this.resize
-		this.w = this.h * this.aspectRatio;
-		this.updatePosition();
-	}
-
-	this.mouseOverUpdate = function() {
-		if(mouse.x > this.absolutex - (this.w / 2) &&
-			mouse.x < this.absolutex + (this.w / 2) &&
-			mouse.y > this.absolutey - (this.h / 2) &&
-			mouse.y < this.absolutey + (this.h / 2)) {
-			if(this.resize < 40) {
-				this.resize += 4;
-				this.resizeUpdate();
-			}
-			if(this.alpha < 1.0) {
-				this.alpha += 0.1;
-			}
-			ctx[currentIndex].globalAlpha = this.alpha;
-			ctx[currentIndex].drawImage(this.info, 
-								 (canvas[currentIndex].width / 2) - 250, 
-								 (canvas[currentIndex].height / 2) - 180, 
-								 500, 
-								 300);
-			ctx[currentIndex].globalAlpha = fadeAlpha;
-		}
-		else if(this.resize > 0) {
-			this.resize -= 2;
-			this.resizeUpdate();
-			this.alpha -= 0.05;
-			ctx[currentIndex].globalAlpha = this.alpha;
-			ctx[currentIndex].drawImage(this.info, 
-								 (canvas[currentIndex].width / 2) - 250, 
-								 (canvas[currentIndex].height / 2) - 180, 
-								 500, 
-								 300);
-			ctx[currentIndex].globalAlpha = fadeAlpha;
-		}	
-	}
-}
 
 document.addEventListener("mousemove", function(e) {
 	mouse.x = e.pageX;
