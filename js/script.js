dcf65946c6fe711387ac18557ba0d3e60b9b5f60
dchen7321 <dchen7321@gmail.com>
2017-04-19 02:43:22 -0400

    Tweak

--- js/script.js
+++ js/script.js
@@ -30,11 +30,11 @@ function CanvasImage(imageID, position, infoID) {
 			this.absolutey = 125;
 		}
 		if(position == 2) {
-			this.absolutex = 380;
+			this.absolutex = 350;
 			this.absolutey = (canvas[0].height / 2) - 25;
 		}
 		if(position == 3) {
-			this.absolutex = canvas[0].width - 380;
+			this.absolutex = canvas[0].width - 350;
 			this.absolutey = (canvas[0].height / 2) - 25;
 		}
 		if(position == 4) {
